ee0dc4fc8c1dd30d865f5986db7ba026e8a55d57
gausszhou <gausszhou@qq.com>
2024-10-12 12:47:57 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+# gausszhou.github.io
---
+++ shells/push.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+cd dist 
+
+git init
+git add .
+
+git remote add origin git@github.com:gausszhou/gausszhou.github.io.git
+
+git checkout -b gh-pages
+time=$(date "+%Y-%m-%d %H:%M")
+git commit -m "docs: update in $time $1"
+git push origin gh-pages -f
+
+cd -
